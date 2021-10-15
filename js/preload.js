6a2036e6c779d75d04dc2fe3a899cc8cd76178c6
Ballsnitz <92341537+Ballsnitz@users.noreply.github.com>
2021-10-15 11:47:01 +1100

    testing a new menu thingy before the level starts

--- js/preload.js
+++ js/preload.js
@@ -63,6 +63,9 @@ PreloadState.prototype.preload = function() {
 	game.load.image("level1","images/level1.png");
 	game.load.image("level2","images/level2.png");
 	game.load.image("level3","images/level3.png");
+	game.load.image("level1Food1","images/lvl1-food1.png");
+	game.load.image("level1Food2","images/lvl1-food2.png");
+	game.load.image("level1Food3","images/lvl1-food3.png");
 	game.load.image('Invisibleground', 'images/Invisibleground.png');
 	game.load.image('invisiblewall500', 'images/invisiblewall500.png');
 	game.load.image("groundWater","images/groundWater.png");
